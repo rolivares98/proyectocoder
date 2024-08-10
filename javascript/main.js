fd0f44adb031583b4fefacfd1870747a47d75aeb
Rodrigo Olivares <rolivares2612@gmail.com>
2024-08-09 21:33:43 -0400

    navbar responsive

--- javascript/main.js
+++ javascript/main.js
@@ -24,3 +24,12 @@ document.getElementById('storyForm').addEventListener('submit', function(event) 
         document.getElementById('storyForm').reset();
     }
 });
+// Funcionalidad para el menú en pantallas pequeñas
+document.addEventListener('DOMContentLoaded', function() {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    navToggle.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+});
