161bde468f2c2137cce559d7e4547d53751173f0
Rodrigo Olivares <rolivares2612@gmail.com>
2024-08-27 22:18:00 -0400

    animacion en las cards

--- javascript/main.js
+++ javascript/main.js
@@ -36,3 +36,18 @@ document.addEventListener('DOMContentLoaded', function() {
 function showImage(src) {
     document.getElementById('lightboxImage').src = src;
 }
+document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.querySelectorAll('.card');
+    
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    });
+  
+    cards.forEach(card => {
+      observer.observe(card);
+    });
+  });
